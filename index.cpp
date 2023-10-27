4863d9e29849982c3f211d3d3f9ffe9082f2f3c0
kateramckay <kateradmckay@gmail.com>
2023-10-26 23:15:03 -0400

    testing

---
+++ index.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+using namespace std;
+
+    void greeting(){
+     string hello = "What is your car of choice?";
+     string afford = "What is your price range";
+
+     cout << hello + "\n" + "\n" + afford;
+    }
+
+    void selection()
+    {
+        
+    }
+
+
+
+int main(){
+
+    greeting();
+
+
+    return 0;
+}
